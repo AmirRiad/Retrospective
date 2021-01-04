c00722062f5d94e540472e6aa9f764b3b6b7b3bc
Amir Riad <amikhael.c@tetco.sa>
2021-01-04 11:05:18 +0200

    Add project files.

---
+++ Retrospective/Controllers/EstimationController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Safeer2.UI.Controllers
+{
+    public class EstimationController : Controller
+    {
+      public  static Dictionary<string, string> AgileTeamWeights = new Dictionary<string, string>() {
+          { "Ali","80" },
+          { "Amir", "80" },
+          { "Ahmed","80" },
+          { "Waad", "80" },
+          { "Keerthi","80" },
+          { "Mallesh", "80" },
+          { "Asif","80" },
+          { "Sarah", "80" },
+          { "Amjad","80" },
+          { "Alhanouf", "80" },
+          { "Bashyer","80" },
+          { "Abeer", "80" },
+          { "Ala","80" }
+      };
+        // GET: Retrospective
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitWeight(string name,string weight)
+        {
+            if (AgileTeamWeights.ContainsKey(name))
+            {
+                AgileTeamWeights.Remove(name);
+            }
+            AgileTeamWeights.Add(name, weight);
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+        public ActionResult GetWeight()
+        {
+            return Json(AgileTeamWeights,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Retrospective/Controllers/VotingController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Safeer2.UI.Controllers
+{
+    public class VotingController : Controller
+    {
+      public  static Dictionary<string, string> AgileTeamVoting = new Dictionary<string, string>() {
+          { "Ali","11" },
+          { "Amir", "11" },
+          { "Ahmed","11" },
+          { "Waad", "11" },
+          { "Keerthi","11" },
+          { "Mallesh", "11" },
+          { "Asif","11" },
+          { "Sarah", "11" },
+          { "Amjad","11" },
+          { "Alhanouf", "11" },
+          { "Bashyer","11" },
+          { "Abeer", "11" },
+          { "Ala","11" }
+      };
+        // GET: Retrospective
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitVoting(string name,string Voting)
+        {
+            if (AgileTeamVoting.ContainsKey(name))
+            {
+                AgileTeamVoting.Remove(name);
+            }
+            AgileTeamVoting.Add(name, Voting);
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+        public ActionResult GetVoting()
+        {
+            return Json(AgileTeamVoting, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
